81385eac9562bee28d3d08c802f37bc17cfd7eb2
Daniel Holmes <senditholmes@gmail.com>
2024-04-09 15:45:09 +0900

    added prisma client, finalised sql database

--- coffee-picker-app/app/page.tsx
+++ coffee-picker-app/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
-
 import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient()
 
+async function retrieveUsers() {
+  const allUsers = await prisma.users.findMany()
+  console.log(allUsers)
+}
+
 export default function Home() {
-  return ( <div className="flex h-96 justify-center content-center items-center flex-1">Hello World</div>);
+  retrieveUsers()
+  return ( <div className="flex h-96 justify-center content-center items-center flex-1">Hello World</div>
+);
 }
